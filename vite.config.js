f7ea9ca6c0f2c4fe31c9341ccdb0b87c47e07381
naomiogundipe <opemipogundipe@gmail.com>
2025-02-16 13:05:17 +0100

    redeploying

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwincss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwincss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  
-  plugins: 
-  [react(),
-  tailwincss()
-  ],
-})
+  plugins: [react(), tailwincss()],
+  base:"/My_Portfolio"
+});
